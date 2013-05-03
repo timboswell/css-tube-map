9a6581417233853fee6ac90879b72c7080f566b4
oakleyarts <oakleyarts@googlemail.com>
2013-05-04 00:23:02 +0100

    Remaining icons styled and applied in mutliple instances using jQuery append

--- js/symbols.js
+++ js/symbols.js
@@ -1,3 +1,8 @@
 $(function(){
 	$(".rail").append("<ul class='rail-symbol'><li class='hor'></li><li class='hor2'></li><li class='diag'></li><li class='diag2'></li><li class='diag3'></li></ul>");
+	$(".disabled").append("<ul class='disabled-symbol'><li class='wheel'></li><li class='legs'></li><li class='arms'></li><li class='thighs'></li><li class='back'></li></ul>");
+	$(".boat").append("<ul class='boat-symbol'><li class='hull'></li><li class='cabin'></li><li class='top'></li><li class='window'></li><li class='window1'></li><li class='window2'></li><li class='window3'></li><li class='wave'></li><li class='wave2'></li><li class='bwave'></li><li class='bwave2'></li><li class='bwave3'></li></ul>");
+	$(".tram").append("<ul class='tram-symbol'><li class='carriage'></li><li class='door1'></li><li class='door2'></li><li class='window1'></li><li class='window2'></li><li class='window3'></li><li class='window4'></li><li class='conductor'></li><li class='wheel1'></li><li class='wheel2'></li><li class='wheel3'></li><li class='wheel4'></li></ul>");
+	$(".plane").append("<ul class='plane-symbol'><li class='fuselage'></li><li class='tail'></li><li class='wing1'></li><li class='wing2'></li></ul>");
+	$(".cross").append("<ul class='cross-symbol'><li class='hor'></li><li class='vert'></li></ul>");
 });
