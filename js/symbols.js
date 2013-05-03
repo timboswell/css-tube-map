fa91b2609856ac46851467492b5572ac627c9fa6
oakleyarts <oakleyarts@googlemail.com>
2013-05-03 08:37:09 +0100

    Symbols created  for Rail, Cable Car and Cross

---
+++ js/symbols.js
@@ -0,0 +1,3 @@
+$(function(){
+	$(".rail").append("<ul class='rail-symbol'><li class='hor'></li><li class='hor2'></li><li class='diag'></li><li class='diag2'></li><li class='diag3'></li></ul>");
+});
